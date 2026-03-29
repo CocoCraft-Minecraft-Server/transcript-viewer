9d410573156c18b78d526a913ba094e8d90ba111
Moonky1 <simon.francoau@gmail.com>
2026-03-28 23:54:26 -0500

    Fix next config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'mongoose'];
+    return config;
+  },
+};
